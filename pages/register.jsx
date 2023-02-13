02a9d49fae14ac7066263c3bb424c74dfaa2c992
lathiesh24 <lathumahi24@gmail.com>
2023-02-13 15:44:44 +0530

    recent

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,32 +1,20 @@
 import React from 'react'
 import { useRouter } from 'next/router';
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
-<<<<<<< HEAD:pages/register.jsx
-import Footer from '../shared/Footer';
-=======
->>>>>>> dcd35c0a98e032190bb23f5b35169389e61becfd:pages/SignReg.jsx
 import Navbar from '../shared/Navbar';
 
 
 const SignReg = () => {
   const router = useRouter();
   const handleSubmit = () => {
-   router.push("/Payment")
+   router.push("/payment")
   }
   return (
-<<<<<<< HEAD:pages/register.jsx
-    <>
-    <Navbar/>
-    <div className="flex justify-center min-h-screen bg-gradient-to-r from-red-200 to-red-500 align-center">
-       <div className='grid justify-center grid-flow-row px-20 py-10 my-8 space-y-8 align-middle bg-white border-2 shadow-lg md:px-36 '>
-         <div className='grid justify-center'>
-=======
    <div>
       <div><Navbar/></div>
       <div className="flex justify-center min-h-screen align-center bg-gradient-to-r from-[#FF9500] via-[#FFC300] to-[#FFEA00] mt-16 rounded-lg" >
        <div className='grid justify-center grid-flow-row px-20 py-10 my-8 space-y-8 align-middle bg-white border-2 rounded-lg shadow-lg md:px-36 '>
          <div className='grid justify-center text-2xl font-bold'>
->>>>>>> dcd35c0a98e032190bb23f5b35169389e61becfd:pages/SignReg.jsx
            Register
          </div>
          <div className=''>
@@ -63,13 +51,8 @@ const SignReg = () => {
             </div>
          </div>
          <div className=''>
-<<<<<<< HEAD:pages/register.jsx
-          <label>
-            Year of Graduation
-=======
           <label className='font-bold'>
-            Year of Study
->>>>>>> dcd35c0a98e032190bb23f5b35169389e61becfd:pages/SignReg.jsx
+            Year of Graduation
           </label>
            <div className='flex flex-row border-b border-slate-400' >
             <div className='mt-1.5 pr-2'>
@@ -95,12 +78,7 @@ const SignReg = () => {
             </div>
        </div>
     </div>
-<<<<<<< HEAD:pages/register.jsx
-    <Footer/>
-    </>
-=======
    </div>
->>>>>>> dcd35c0a98e032190bb23f5b35169389e61becfd:pages/SignReg.jsx
   )
 }
 
