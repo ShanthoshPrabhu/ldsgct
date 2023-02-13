dde1d90df4ab6ddc19a3c08db69e927c7d1882c8
lathiesh24 <lathumahi24@gmail.com>
2023-02-14 04:48:41 +0530

    recent

--- pages/OnlinePay.jsx
+++ pages/OnlinePay.jsx
@@ -100,7 +100,7 @@ Once the payment is done, take a screenshot of the payment in a manner that the 
         </div>
         <div  className='flex flex-col items-center justify-center p-10 mx-12 bg-gray-800 border rounded-lg shadow-md shadow-yellow-500 w-[280px] md:w-[300px] h-120'>
           <img src="https://www.linkpicture.com/q/IMG-20230214-WA0002.jpg" alt="verfication" height="100" width="200"/>
-          <p className="mt-2 text-xl font-semibold text-center text-yellow-500">Upload your payment screenshot for verification</p>
+          <p className="mt-2 text-xl font-normal text-center text-yellow-500">Upload your payment screenshot for verification with you UPI transaction ID and Name</p>
         </div>
         </div>
        </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,8 @@ export default function Home() {
     <div>
       <Head>
         <title>Literary and Debating Society</title>
-         
+         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+         <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
       </Head>
 
       <main>
