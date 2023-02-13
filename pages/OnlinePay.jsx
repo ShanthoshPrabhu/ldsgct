4fd39c334c3277a7635451d0e7641b394f684704
lathiesh24 <lathumahi24@gmail.com>
2023-02-14 04:25:35 +0530

    recent

--- pages/OnlinePay.jsx
+++ pages/OnlinePay.jsx
@@ -49,14 +49,14 @@ async function uploadData(){
   return (
     <> 
     <Navbar/>
-    <div className='flex items-center justify-center min-h-screen bg-gradient-to-r from-[#FF9500] via-[#FFC300] to-[#FFEA00] mt-16'>   
+    <div className='flex items-center justify-center min-h-screen bg-white mt-16'>   
      <div className='container' >
-        <div className='flex items-center justify-center h-16 mt-10 text-3xl font-light text-red-700 md:text-5xl md:font-base'>
+        <div className='flex items-center justify-center h-16 mt-10 text-3xl font-light text-gray-800 md:text-5xl md:font-base'>
             Online Mode Payment
        </div>
       
        <div class=" text-center pt-4 lg:px-4 rounded-lg w-lg mt-6">
-          <div class="p-2 bg-red-600 items-center text-indigo-100 leading-none rounded-lg flex lg:inline-flex mx-10 md:mx-0" role="alert">
+          <div class="p-2 bg-yellow-600 items-center text-white leading-none rounded-lg flex lg:inline-flex mx-10 md:mx-0" role="alert">
             <span class="flex rounded-full bg-gray-800 uppercase px-2 py-1 text-xs font-bold mr-3">Pay</span>
             <span class="font-semibold mr-2 text-left flex-auto">!!Use any one of the below payment methods to make the payment 
 As you have availed the early bird offer, pay Rs 150 (applicable until 18/02/2023)
@@ -69,25 +69,25 @@ Once the payment is done, take a screenshot of the payment in a manner that the 
       <div className='grid justify-around grid-flow-row md:grid-flow-col'>
       <div 
       className='grid justify-center grid-flow-row cursor-pointer md:grid-flow-row gap-y-24 md:gap-x-24 mt-14 md:mt-10 '>
-       <div className='flex flex-col items-center justify-center h-48 p-6 mx-12 bg-gray-100 border rounded-lg shadow-2xl sm:h-56 w-72 sm:w-96 '>
+       <div className='flex flex-col items-center justify-center h-48 p-6 mx-12 bg-gray-800 border rounded-lg shadow-md shadow-yellow-500 sm:h-56 w-72 sm:w-96 '>
         <img src="/qr.jpeg" alt="QR_Code" className="overflow-hidden"/>
-         <div className='font-bold text-center' >Scan the QR Code to pay</div>
+         <div className='font-light text-center text-yellow-500 mt-4' >Scan the QR Code to pay</div>
         </div>
-        <div className='flex flex-col items-center justify-center h-48 mx-12 bg-white border rounded-lg shadow-2xl sm:h-56 w-72 sm:w-96'>
+        <div className='flex flex-col items-center justify-center h-48 mx-12 bg-gray-800 border rounded-lg shadow-md shadow-yellow-500 sm:h-56 w-72 sm:w-96'>
         <img src="/UPI-Color.png" alt="" width="150" height="150" className="mb-4"/>
-        <a href="harishhar2304@oksbi" className="mb-4 text-blue-500"> harishhar2304@oksbi </a>
-         <div className='font-bold text-center'>Click to pay through UPI Id</div>
+        <a href="harishhar2304@oksbi" className="mb-4 text-light text-yellow-500 "> harishhar2304@oksbi </a>
+         <div className='font-bold text-center text-yellow-500'>Click to pay through UPI Id</div>
         </div>
-        <div className='flex flex-col items-center justify-center h-48 mx-12 bg-white border rounded-lg shadow-2xl sm:h-56 w-72 sm:w-96'>
-        <div className='mb-4 text-xl font-bold text-center text-blue-500'>9894578288</div>
-         <div className='h-10 font-bold text-center'>Pay through Phone Number</div>
-         <div className='h-10 font-light text-center'>You can pay the registration fee to this number</div>
+        <div className='flex flex-col items-center justify-center h-48 mx-12 bg-gray-800 border rounded-lg shadow-md shadow-yellow-500 sm:h-56 w-72 sm:w-96'>
+        <div className='mb-4 text-xl font-light text-center text-yellow-500'>9894578288</div>
+         <div className='h-10 font-bold text-center text-yellow-500'>Pay through Phone Number</div>
+         <div className='h-10 font-light text-center text-white'>You can pay the registration fee to this number</div>
          
         </div>
        </div> 
         <div className='grid justify-center grid-flow-row cursor-pointer md:grid-flow-row gap-y-24 md:gap-x-24 mt-14 md:mt-10'>
-       <div className='flex flex-col items-center justify-center h-48 p-10 mx-12 bg-gray-100 border rounded-lg shadow-2xl sm:h-56 w-72 sm:w-96'>
-       <input type="file" id="file-input"  onChange={(e)=>addImage(e)} className="mx-auto md:mx-auto md:ml-8" />
+       <div className='flex flex-col items-center justify-center h-48 p-10 mx-12 bg-gray-800 border rounded-lg shadow-md shadow-yellow-500 sm:h-56 w-72 sm:w-96'>
+       <input type="file" id="file-input"  onChange={(e)=>addImage(e)} className="mx-auto md:mx-auto md:ml-8 text-yellow-500" />
         <button  
         className=" text-white mt-5 bg-gradient-to-r  from-[#FF9500] via-[#FFC300] to-[#FFEA00] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
         onClick={uploadData} 
@@ -98,9 +98,9 @@ Once the payment is done, take a screenshot of the payment in a manner that the 
           </div>
            )}
         </div>
-        <div  className='flex flex-col items-center justify-center p-10 mx-12 bg-gray-100 border rounded-lg shadow-2xl w-[280px] md:w-[300px] h-120'>
+        <div  className='flex flex-col items-center justify-center p-10 mx-12 bg-gray-800 border rounded-lg shadow-md shadow-yellow-500 w-[280px] md:w-[300px] h-120'>
           <img src="/verify.jpeg" alt="verfication" height="100" width="200"/>
-          <p className="mt-2 text-xl font-semibold text-center ">Upload your payment screenshot for verification</p>
+          <p className="mt-2 text-xl font-semibold text-center text-yellow-500">Upload your payment screenshot for verification</p>
         </div>
         </div>
        </div>
