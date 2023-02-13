9ac3bc22b83bc9fd7d916ed4e487ee0b4f39baa1
lathiesh24 <lathumahi24@gmail.com>
2023-02-13 17:25:58 +0530

    recent

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,16 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Footer from "../shared/Footer";
 import Navbar from "../shared/Navbar";
 
 export default function Home() {
+  const router = useRouter();
+  const handleClickPic = () => {
+     router.push("/event/Events")
+  }
   return (
+
     <div>
       <Head>
         <title>Literary and Debating Society</title>
@@ -22,7 +28,7 @@ export default function Home() {
 
       <main>
        <Navbar/>
-      <div className='grid grid-cols-3 mt-20'>
+      <div className='grid mt-20 md:grid-cols-3 '>
          <div className="px-10 col-span-2 text-lg font-light leading-loose flex-col justify-center items-center">
           <p className="">One of Coimbatore's largest events dedicated solely to celebrating the love of the English Language and its rich literature is back and ready to be the literary festival that will leave you in awe.</p>
           <p className="">Over the years, Brainstrain has established itself as a grand arena where more than 200 literary intellectuals from across Coimbatore and other parts of the state come together. This event has its grand vision set fast on bringing together students from different disciplines and all walks of life to foster the spirit of friendly competition among peers while having fun in the various exciting events that they will be participating in.</p>
@@ -31,7 +37,7 @@ export default function Home() {
           <p className="">And that's not all! This 2023, Brain strain promises to be an even more of a spectacular venue with an extravagant and sizzling lineup of events that are sure to set the arena ablaze. From debating your way out of a sinking ship and bringing your words to life, gear up as you traverse through various events and display your flair to captivate the eager spectators.</p>
       </div>
          <div>
-          <img src="" alt="Go to Brainstrain"/>
+          <img src="https://www.linkpicture.com/q/coming-soon5.jpg" alt="Go to Brainstrain" className="flex justify-center items-center max-h-96 mt-16 ml-16 mb-20 cursor-pointer" onClick={handleClickPic} />
          </div>
       </div>
       <Footer/>
